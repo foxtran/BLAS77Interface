06855edf06b7a4db3e79fabff071c70d98113b4b
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-12-02 08:26:40 +0100

    Fix example1

--- example/example1.f90
+++ example/example1.f90
@@ -3,16 +3,7 @@ program test_blas
    implicit none
    real :: dot, snorm, x(3) = [1.0, 2.0, 3.0], y(3) = [3.0, 0.0, 4.0]
    character(*), parameter :: cfmt='(a,i0,2(a,f0.3))'
-   block
-     integer, parameter :: wp=kind(1.e0)
-     dot = sdot(3,x,1,y,1)
-     snorm = snrm2(3,y,1)
-     write(*,cfmt) 'wp kind=', wp, ' sdot=', dot, ' snrm2=', snorm
-   end block
-   block
-     integer, parameter :: wp=kind(1.d0)
-     dot = sdot(3,x,1,y,1)
-     snorm = snrm2(3,y,1)
-     write(*,cfmt) 'wp kind=', wp, ' sdot=', dot, ' snrm2=', snorm
-   end block
+   dot = sdot(3,x,1,y,1)
+   snorm = snrm2(3,y,1)
+   write(*,cfmt) ' sdot=', dot, ' snrm2=', snorm
 end program test_blas
